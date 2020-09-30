a320793eb8431a3eb9900d67d8b980d9592e5adf
Piotr <piotr.w.zagorski@gmail.com>
2020-09-30 11:15:29 +0200

    form update

--- src/Controller/Project/ProjectAdd.php
+++ src/Controller/Project/ProjectAdd.php
@@ -2,10 +2,20 @@
 namespace App\Controller\Project;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProjectAdd extends AbstractController{
 
     function index(){
+        return $this->render('project/project_add.html.twig');
+    }
+
+    function addProject(Request $request){
+        dump($request->getContent());
+
+
+
+
         return $this->render('project/project_add.html.twig');
     }
 }
