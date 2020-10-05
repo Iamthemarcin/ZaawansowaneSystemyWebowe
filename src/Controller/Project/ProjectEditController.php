2c82b4ef76849fe2d94338b32feaf38e44a4281e
Piotr <piotr.w.zagorski@gmail.com>
2020-10-05 14:49:48 +0200

    edition template refactor

--- src/Controller/Project/ProjectEditController.php
+++ src/Controller/Project/ProjectEditController.php
@@ -1,11 +1,17 @@
 <?php
 namespace App\Controller\Project;
 
+use App\Entity\Client\ClientEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProjectEditController extends AbstractController{
 
     function index(){
-        return $this->render('@Project/project_edit.html.twig');
+
+        $clients = $this->getDoctrine()->getRepository
+        (ClientEntity::class)->findAll();
+        return $this->render('@Project/project_edit.html.twig', [
+            'clients'=>$clients
+        ]);
     }
 }
