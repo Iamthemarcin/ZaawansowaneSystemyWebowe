d0f379451e4f50a686f23aa35295bb1e0d442bcf
Marcin M <Iamthemarcin@wp.pl>
2020-10-08 10:22:59 +0200

    lol

--- src/Controller/Project/ProjectViewController.php
+++ src/Controller/Project/ProjectViewController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProjectViewController extends AbstractController{
 
     function index(){
+
         return $this->render('project/project_view.html.twig');
     }
 }
