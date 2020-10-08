79a876b9b2aed5763aee8b5bbe2aa8a2020bedae
Piotr <piotr.w.zagorski@gmail.com>
2020-10-08 09:48:18 +0200

    test entity

---
+++ src/Entity/SpeedTestEntity.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SpeedTestEntityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SpeedTestEntityRepository::class)
+ */
+class SpeedTestEntity
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $projectId;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $linkId;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateTime;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $mobileAvg;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $desktopAvg;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $JSON;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProjectId(): ?int
+    {
+        return $this->projectId;
+    }
+
+    public function setProjectId(?int $projectId): self
+    {
+        $this->projectId = $projectId;
+
+        return $this;
+    }
+
+    public function getLinkId(): ?int
+    {
+        return $this->linkId;
+    }
+
+    public function setLinkId(?int $linkId): self
+    {
+        $this->linkId = $linkId;
+
+        return $this;
+    }
+
+    public function getDateTime(): ?\DateTimeInterface
+    {
+        return $this->dateTime;
+    }
+
+    public function setDateTime(?\DateTimeInterface $dateTime): self
+    {
+        $this->dateTime = $dateTime;
+
+        return $this;
+    }
+
+    public function getMobileAvg(): ?string
+    {
+        return $this->mobileAvg;
+    }
+
+    public function setMobileAvg(?string $mobileAvg): self
+    {
+        $this->mobileAvg = $mobileAvg;
+
+        return $this;
+    }
+
+    public function getDesktopAvg(): ?string
+    {
+        return $this->desktopAvg;
+    }
+
+    public function setDesktopAvg(?string $desktopAvg): self
+    {
+        $this->desktopAvg = $desktopAvg;
+
+        return $this;
+    }
+
+    public function getJSON(): ?string
+    {
+        return $this->JSON;
+    }
+
+    public function setJSON(?string $JSON): self
+    {
+        $this->JSON = $JSON;
+
+        return $this;
+    }
+}
---
+++ src/Repository/SpeedTestEntityRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\SpeedTestEntity;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method SpeedTestEntity|null find($id, $lockMode = null, $lockVersion = null)
+ * @method SpeedTestEntity|null findOneBy(array $criteria, array $orderBy = null)
+ * @method SpeedTestEntity[]    findAll()
+ * @method SpeedTestEntity[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SpeedTestEntityRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, SpeedTestEntity::class);
+    }
+
+    // /**
+    //  * @return SpeedTestEntity[] Returns an array of SpeedTestEntity objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?SpeedTestEntity
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
