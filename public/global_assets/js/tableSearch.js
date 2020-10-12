2f634af33cca596287fcf79925590d6ff9649b99
Marcin M <Iamthemarcin@wp.pl>
2020-10-12 15:45:47 +0200

    lol

--- public/global_assets/js/tableSearch.js
+++ public/global_assets/js/tableSearch.js
@@ -20,5 +20,56 @@ function tableSearch() {
             }
         }
     }
+}
+
+
+function statusSearch() {
+    var filter, table, tr, td, i, cell, j;
+
+    filter = 'NIEAKTYWNA'
+    table = document.getElementById("myStatusTable");
+    tr = table.getElementsByTagName("tr");
+
+    if (undo == true){
+        undo = false;
+        for (i = 1; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td");
+            for (j = 0; j < td.length; j++) {
+                tr[i].style.display = "";
+            }
+        }
+    }
+
+
+
+
+else{
+
+        console.log(undo)
+        undo = true;
+        for (i = 1; i < tr.length; i++) {
+
+
+
+
+
+            td = tr[i].getElementsByTagName("td");
+            for (j = 0; j < td.length; j++) {
+                cell = tr[i].getElementsByTagName("td")[j];
+
+                if (cell) {
+                    console.log('wtF');
+                    if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "none";
+
+                        break;
+
+                    }
+                }
+            }
+        }
+
+    }
+
 
 }
