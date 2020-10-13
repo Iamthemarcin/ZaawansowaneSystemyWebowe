163356f9108bc547f2d188a0411d61e64e063fc2
Marcin M <Iamthemarcin@wp.pl>
2020-10-13 11:22:49 +0200

    ladniej lol przerwa chyba co???? nie no zartuje

--- public/global_assets/js/tableSearch.js
+++ public/global_assets/js/tableSearch.js
@@ -23,20 +23,7 @@ function tableSearch() {
 }
 
 
-function ActiveStatusSearch() {
-    var filter, table, tr, td, i, cell, j;
 
-    filter = 'NIEAKTYWNA'
-    table = document.getElementById("myStatusTable");
-    tr = table.getElementsByTagName("tr");
-
-        for (i = 1; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td");
-            for (j = 0; j < td.length; j++) {
-                tr[i].style.display = "";
-            }
-        }
-    }
 function FilterStatus(filter) {
     // Declare variables
     var Status_column, filter, table, tr, td, i, txtValue, ths, th;
@@ -74,31 +61,3 @@ function FilterStatus(filter) {
 }
 
 
-function InactiveStatusSearch() {
-    var filter, table, tr, td, i, cell, j;
-
-    filter = 'NIEAKTYWNA'
-    table = document.getElementById("myStatusTable");
-    tr = table.getElementsByTagName("tr");
-
-        console.log(undo)
-        undo = true;
-        for (i = 1; i < tr.length; i++) {
-
-
-            td = tr[i].getElementsByTagName("td");
-            for (j = 0; j < td.length; j++) {
-                cell = tr[i].getElementsByTagName("td")[j];
-
-                if (cell) {
-                    console.log('wtF');
-                    if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                        tr[i].style.display = "none";
-
-                        break;
-
-                    }
-                }
-            }
-        }
-}
