b880c0f5af1e80ad822ed915ba0b7735c066de4f
Piotr <piotr.w.zagorski@gmail.com>
2020-10-19 09:48:06 +0200

    ajax test

---
+++ public/assets/js/minuteTest.js
@@ -0,0 +1,19 @@
+function showMinuteTestData(ids) {
+//ids[0] test link ids[1] project link
+
+
+    document.getElementById("testCount ").innerHTML = ids[1];
+    // var xhttp;
+    // if (str == "") {
+    //     document.getElementById("txtHint").innerHTML = "";
+    //     return;
+    // }
+    // xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         document.getElementById("txtHint").innerHTML = this.responseText;
+    //     }
+    // };
+    // xhttp.open("GET", "getcustomer.php?q="+str, true);
+    // xhttp.send();
+}
