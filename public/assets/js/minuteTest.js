8efd59b30e7a0b48c5ca4df0926f55eaffce569d
Piotr <piotr.w.zagorski@gmail.com>
2020-10-20 08:25:44 +0200

    x

--- public/assets/js/minuteTest.js
+++ public/assets/js/minuteTest.js
@@ -3,17 +3,19 @@ function showMinuteTestData(ids) {
 
 
     document.getElementById("testCount ").innerHTML = ids[1];
-    // var xhttp;
-    // if (str == "") {
-    //     document.getElementById("txtHint").innerHTML = "";
-    //     return;
-    // }
-    // xhttp = new XMLHttpRequest();
-    // xhttp.onreadystatechange = function() {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //         document.getElementById("txtHint").innerHTML = this.responseText;
-    //     }
-    // };
-    // xhttp.open("GET", "getcustomer.php?q="+str, true);
-    // xhttp.send();
+
+    var xhttp;
+    if (ids == "") {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    }
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("txtHint").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "getMinuteDate.php?q="+ids[1], true);
+    xhttp.send();
+
 }
