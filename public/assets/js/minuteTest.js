7acf3d058454e8ceacd7a5e24c2d8ec514073de5
Piotr <piotr.w.zagorski@gmail.com>
2020-10-23 09:00:29 +0200

    chart fixation

--- public/assets/js/minuteTest.js
+++ public/assets/js/minuteTest.js
@@ -127,32 +127,29 @@ function showMinuteTestData(ids) {
                             labels: chartdata1,
                             datasets: [{
                                 data: chartdata2,
-                                backgroundColor: [
-                                    'rgba(255, 99, 132, 0.2)',
-                                    'rgba(54, 162, 235, 0.2)',
-                                    'rgba(255, 206, 86, 0.2)',
-                                    'rgba(75, 192, 192, 0.2)',
-                                    'rgba(153, 102, 255, 0.2)',
-                                    'rgba(255, 159, 64, 0.2)'
-                                ],
-                                borderColor: [
-                                    'rgba(255, 99, 132, 1)',
-                                    'rgba(54, 162, 235, 1)',
-                                    'rgba(255, 206, 86, 1)',
-                                    'rgba(75, 192, 192, 1)',
-                                    'rgba(153, 102, 255, 1)',
-                                    'rgba(255, 159, 64, 1)'
-                                ],
+                                backgroundColor: 'rgb(20,14,207,0.2)',
+                                borderColor: 'rgb(20,14,207)',
                                 borderWidth: 1
                             }]
                         },
-                        options: {
-                            scales: {
-                                yAxes: [{
-                                    ticks: {
-                                        beginAtZero: true
+                        options: {  responsive: true,
+                            maintainAspectRatio: false,
+                            tooltips: {
+                            enabled: false
+                            },
+                            scales:{
+                            xAxes:[{
+                                type: 'time',
+                                time:{
+                                    minUnit: 'minute',
+                                displayFormats: {
+                                        day : 'MMM D'
                                     }
-                                }]
+                                    },
+                                ticks:{
+
+                                }
+                            }]
                             }
                         }
                     });
