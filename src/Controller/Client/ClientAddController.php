3a89ae6bfa6d5918a64dc2bd11c7b5bda6d85a04
Marcin M <Iamthemarcin@wp.pl>
2020-12-09 12:22:01 +0100

    All files

--- src/Controller/Client/ClientAddController.php
+++ src/Controller/Client/ClientAddController.php
@@ -8,9 +8,11 @@ use App\Entity\Client\ClientEntity;
 use App\Form\Client\ClientAddType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Validation;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class ClientAddController extends AbstractController
 {
@@ -23,12 +25,28 @@ class ClientAddController extends AbstractController
     ){
         $this->clientBuilder = $clientBuilder;
         $this->em = $em;
+
     }
 
 
-
-
     public function index(Request $request){
+
+            $httpClient = HttpClient::create();
+            try {
+                $response = $httpClient->
+                request('GET',
+                    'https://joke3.p.rapidapi.com/v1/joke',
+                    [
+                        'headers' => [
+                            'x-rapidapi-key' => 'c8b99395b2msh322953e25c303d8p184f58jsn6570ddee2dfd',
+                            'x-rapidapi-host' => 'joke3.p.rapidapi.com'
+                        ]
+                    ]);
+            } catch (TransportExceptionInterface $e) {
+            }
+
+            $content = $response->toArray();
+            $content = $content['content'];
 
             $form = $this->createForm(ClientAddType::class);
             $form->handleRequest($request);
@@ -65,6 +83,8 @@ class ClientAddController extends AbstractController
 
             return $this->render('@Client/client_add.html.twig', [
                 'form' => $form->createView(),
+                'dowcip' => $content,
+
                 #WALIDACJA
                 'errors' => $errors,
 
